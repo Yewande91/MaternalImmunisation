bf5a0c321552f0d74a242c216e58f45e781b1a3c
Adeleke <ya213@ic.ac.uk>
2020-07-16 16:24:06 +0100

    Changed load script to match dummy data set columns

--- R/Load.R
+++ R/Load.R
@@ -1,3 +1,6 @@
+#install the following packages
+install.packages(c("magrittr","lubridate","tidyverse"))
+
 #load magrittr
 library(magrittr)
 
@@ -7,7 +10,6 @@ library(lubridate)
 #load tidyverse
 library(tidyverse)
 
-
 #find file names with a data-raw/Matimms_Data_extension
 fileNames<-Sys.glob("data-raw/Matimms_Data_*.csv")
 
@@ -16,14 +18,29 @@ load_data_files<-function(fN){
   lapply(fN, function(x)
   {loadfiles<-read_csv(x,
                        col_types=cols(
-                         'Patient key'= col_integer(),
-                         'Age' = col_integer(),
-                         'Ethnicity' = col_character(),
-                         'Diabetes' = col_date(format = ""),
-                         'Kidney disease' =	col_date(format = ""),
-                         'CCG' =	col_character(),
-                         'Pregstart' =	col_date(format = ""),
-                         'Pregend'= col_date(format = ""))
+                         'mother_wsic_id'= col_integer(),
+                         'preg_episode_id' = col_guess(), 
+                         'preg_start_date' = col_date(format = ""),
+                         'preg_end_date' = col_date(format = ""),
+                         'mother_age_sop' = col_integer(),
+                         'ethnicity' = col_factor(levels = c("Asian or asian british",
+                                                             "Black or black british", 
+                                                             "Mixed", 
+                                                             "White",
+                                                             "UNKNOWN",
+                                                             "Other ethnic groups",
+                                                             "Not known")),
+                         'local_authority' =	col_character(),
+                         'asthma' =	col_integer(),
+                         'chr_respiratory_disease' =	col_integer(),
+                         'chr_heart_disease'  =	col_integer(),
+                         'chr_kidney_disease' =	col_integer(),
+                         'chr_liver_disease' =	col_integer(),
+                         'asplenia_or_dys_spleen' =	col_integer(),
+                         'chr_neurological_disease' =	col_integer(),
+                         'diabetes' =	col_integer(),
+                         'immunosuppression' =  col_integer(),
+                         'morbid_obesity'=  col_integer())
   )
   })
 }
@@ -31,5 +48,6 @@ load_data_files<-function(fN){
 #create variable called data_frames which contains the list of dataframes 
 #with the data-raw/Matimms_Data_extension
 data_frames <- load_data_files(fileNames)
+
 
 
